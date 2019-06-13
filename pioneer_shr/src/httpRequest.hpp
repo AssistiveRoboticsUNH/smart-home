b0fde73ca2ec1bfbe136c66de89daf8d75e63abf
Tianyi Gu <2008tygu@gmail.com>
2019-06-13 10:39:38 -0400

    update http request to read our own jason list style

--- pioneer_shr/src/httpRequest.hpp
+++ pioneer_shr/src/httpRequest.hpp
@@ -1,19 +1,37 @@
 #include <cpr/cpr.h>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 #include "jasonParser.hpp"
 
 class HttpRequest {
 public:
     bool isOpen() {
-        auto r = cpr::Get(cpr::Url{"http://www.httpbin.org/get"});
+        auto r = cpr::Get(cpr::Url{"http://localhost:4567/getcontacts"});
+
+        std::string webtext = r.text;
+
+        std::string jasonListStr = webtext.substr(1, webtext.size() - 2);
+
+        std::stringstream ss(jasonListStr);
+
+        std::vector<std::string> jasonStrList;
+
+        std::string jasonStr;
+
+        while (getline(ss, jasonStr, ',')) {
+            jasonStrList.push_back(jasonStr);
+        }
 
         std::cout << "parser by tianyi:  " << std::endl;
 
-        JasonParser jason(r.text);
+        // JasonParser jason(r.body);
 
-        std::cout << jason.getValue("origin") << std::endl;
+        // std::cout << jason.getValue("origin") << std::endl;
 
-		return true;
+        std::cout << jasonStrList[0] << std::endl;
+
+        return true;
     }
 };
