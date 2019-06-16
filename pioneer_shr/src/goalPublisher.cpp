ea794645a7216b6c1fe83b9fb02b065a38ba5773
Tianyi Gu <2008tygu@gmail.com>
2019-06-16 18:14:25 -0400

    write goal position in launch file

--- pioneer_shr/src/goalPublisher.cpp
+++ pioneer_shr/src/goalPublisher.cpp
@@ -55,42 +55,59 @@ public:
 int main(int argc, char** argv){
   ros::init(argc, argv, "simple_navigation_goals");
 
+  ros::NodeHandle nh("~");
+
+  float x,y,z,w;
+
+  nh.getParam("goalPosePositionX", x);
+  nh.getParam("goalPosePositionY", y);
+  nh.getParam("goalPoseOrientationZ", z);
+  nh.getParam("goalPoseOrientationW", w);
+
+  std::cout << "load in goalPosePositionX: " << x << std::endl;
+  std::cout << "load in goalPosePositionY: " << y << std::endl;
+  std::cout << "load in goalPoseOrientationZ: " << z << std::endl;
+  std::cout << "load in goalPoseOrientationW: " << w << std::endl;
+
   AutoNavigation autonav;
 
   geometry_msgs::Pose goalPose;
-  /*goalPose.position.x = 1.72;*/
-  //goalPose.position.y = 1.52;
-  //goalPose.orientation.z = 0.73;
-  /*goalPose.orientation.w = 0.67;*/
+  //goalPose.position.x = 1.72;
+  goalPose.position.x = x;
+  goalPose.position.y = y;
+  goalPose.orientation.z = z;
+  goalPose.orientation.w = w;
 
-  goalPose.position.x = -0.6109;
-  goalPose.position.y = -1.7612;
-  goalPose.orientation.z = 0.9947;
-  goalPose.orientation.w = -0.1019;
+  //goalPose.position.x = -0.6109;
+  //goalPose.position.y = -1.7612;
+  //goalPose.orientation.z = 0.9947;
+  //goalPose.orientation.w = -0.1019;
 
-  HttpRequest httpReq;
+  //HttpRequest httpReq;
 
   //we don't need this if there is a nodehandle be created
-  ros::Time::init();
+  //ros::Time::init();
 
   ros::Rate loop_rate(10);
   loop_rate.sleep();
   ros::spinOnce();
 
-  bool doorOpen = false;
+  //bool doorOpen = false;
 
-  while (ros::ok()) {
-      if (doorOpen)
-          break;
+  //while (ros::ok()) {
+      //if (doorOpen)
+          //break;
 
-      ros::spinOnce();
+      //ros::spinOnce();
 
-      if (httpReq.isOpen()) {
-          autonav.navigateTo(goalPose);
-		  autonav.playAudio();
-		  doorOpen = true;
-      }
-  }
+      //if (httpReq.isOpen()) {
+          //autonav.navigateTo(goalPose);
+		  //autonav.playAudio();
+		  //doorOpen = true;
+      //}
+  /*}*/
+
+  autonav.navigateTo(goalPose);
 
   return 0;
 }
