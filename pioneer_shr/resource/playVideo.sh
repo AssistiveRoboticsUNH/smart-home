f9358e59940f44a722f3928ae55d4f8c9692e01d
Tianyi Gu <2008tygu@gmail.com>
2019-06-24 15:23:50 -0400

    update video background play, enable do it through ssh

--- pioneer_shr/resource/playVideo.sh
+++ pioneer_shr/resource/playVideo.sh
@@ -1,1 +1,3 @@
-vlc ${ROS_WORKSPACE}/src/pioneer_shr/resource/warning-video.mkv &
+export DISPLAY=:0
+(vlc ${ROS_WORKSPACE}/src/pioneer_shr/resource/warning-video.mkv; killall -9 vlc; eog --fullscreen ${ROS_WORKSPACE}/src/pioneer_shr/resource/big_white_face.jpg) &
+
