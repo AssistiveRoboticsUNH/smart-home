42b8e2f08b68be0034db73df3bf3496b7820ccaf
Paul Gesel <pac48@wildcats.unh.edu>
2022-09-19 20:02:07 -0400

    added robot description

---
+++ pioneer_description/launch/view.launch.py
@@ -0,0 +1,54 @@
+from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+
+def generate_launch_description():
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("pioneer_description"),
+                    "urdf",
+                    "pioneer3dx.xacro",
+                ]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": ParameterValue(robot_description_content, value_type=str)
+}
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("pioneer_description"), "rviz", "view_robot.rviz"]
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+    #
+    nodes_to_start = [
+        joint_state_publisher_node,
+        robot_state_publisher_node,
+        rviz_node,
+    ]
+
+    return LaunchDescription(nodes_to_start)
