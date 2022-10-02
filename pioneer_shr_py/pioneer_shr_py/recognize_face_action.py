86f8e7cbaab2a22b470a631bef546366d1630468
Paul Gesel <pac48@wildcats.unh.edu>
2022-10-02 13:18:46 -0400

    working face action server

--- pioneer_shr_py/pioneer_shr_py/recognize_face_action.py
+++ pioneer_shr_py/pioneer_shr_py/recognize_face_action.py
@@ -4,7 +4,7 @@ import time
 
 from ament_index_python.packages import get_package_share_directory
 from cv_bridge import CvBridge, CvBridgeError
-from pioneer_shr_msg.action import EmptyRequest
+from pioneer_shr_msg.action import RecognizeTrainRequest, RecognizeRequest
 from rclpy.action import ActionServer, ActionClient
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -13,48 +13,181 @@ from sound_play_msg.action import SoundRequest
 import face_recognition
 import rclpy
 
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+
+recognize_train_timeout = 30
+recognize_timeout = 2
+save_path = os.path.join(os.path.expanduser('~'), '.smart-home')
+camera_topic = '/camera/rgb/image_raw'
+voice = 'voice_cmu_us_fem_cg'
+
+
+# voice = 'voice_kal_diphone'
+# voice = 'voice_cmu_us_fem_cg'
+# voice = 'voice_cmu_us_aew_cg'
 
 class RecognizeFaceActionServer(Node):
-
     def __init__(self):
         super().__init__('recognize_face_action')
-        self._action_server = ActionServer(
-            self,
-            EmptyRequest,
-            'recognize_face',
-            self.recognize_callback)
 
-        # Ordered this way to minimize wait time.
-        soundhandle = SoundClient(self, blocking=True)
+        self.training_action = False
+        self.recognize_action = False
+        self.latest_image = np.array([0])
+        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        self.br = CvBridge()
 
-        # voice = 'voice_kal_diphone'
-        voice = 'voice_cmu_us_fem_cg'
-        # voice = 'voice_cmu_us_aew_cg'
-        volume = 1.0
+        self.sub = self.create_subscription(Image, camera_topic, self.image_callback, 1)
+        self.train_action_server = ActionServer(self, RecognizeTrainRequest, 'train_recognize_face',
+                                                self.train_callback)
+        self.recognize_action_server = ActionServer(self, RecognizeRequest, 'recognize_face', self.recognize_callback)
 
-        s = 'Make sure you are centered in the camera frame'
-        # s = 'Hi.    How are you today?'
+        self.soundhandle = SoundClient(self, blocking=True)
 
-        self.get_logger().info('Saying: %s' % s)
-        self.get_logger().info('Voice: %s' % voice)
-        self.get_logger().info('Volume: %s' % volume)
-
-        soundhandle.say(s, voice, volume)
-
-        get_package_share_directory('pioneer_shr_msg')
+    def image_callback(self, data):
+        if self.training_action or self.recognize_action:
+            current_frame = self.br.imgmsg_to_cv2(data)
+            self.latest_image = cv2.cvtColor(current_frame, cv2.COLOR_BGR2RGB)
+            self.latest_image_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+        else:
+            self.latest_image = np.array([0])
 
     def recognize_callback(self, goal_handle):
-        self.get_logger().info('Executing goal...')
-        result = EmptyRequest.Result()
+        self.get_logger().info('Recognize face...')
+        feedback_msg = RecognizeRequest.Feedback()
+        database = self.get_database()
+        start_time = time.time()
+
+        self.recognize_action = True
+        standing_person_face_encoding = None
+        while time.time() - start_time < recognize_timeout:
+            if self.latest_image.shape[0] > 1:
+                standing_person_face_encoding = self.detect_face(self.latest_image)
+                if standing_person_face_encoding is not None:
+                    break
+            feedback_msg.running = True
+            goal_handle.publish_feedback(feedback_msg)
+            rclpy.spin_once(self)
+
+        self.recognize_action = False
+
+        result = RecognizeRequest.Result()
+        if standing_person_face_encoding is not None:
+            goal_handle.succeed()
+            for encoding in standing_person_face_encoding:
+                min_val = np.finfo(float).max
+                min_name = ""
+                for name in database:
+                    tmp = np.sum(np.abs(database[name] - encoding), axis=1)
+                    if np.min(tmp) < min_val:
+                        min_val = np.min(tmp)
+                        min_name = name
+
+                result.names.append(min_name)
+        else:
+            goal_handle.abort()
+            result.names = [""]
+
         return result
 
     def train_callback(self, goal_handle):
-        pass
-        # Load a sample picture and learn how to recognize it.
-        # standing_person_image = face_recognition.load_image_file(image_path)
-        # standing_person_face_encoding = face_recognition.face_encodings(standing_person_image)[0]
+        self.get_logger().info('Train recognize face...')
+        start_time = time.time()
+        feedback_msg = RecognizeTrainRequest.Feedback()
 
-        # return standing_person_face_encoding
+        self.training_action = True
+        center_prompt_given = False
+        only_person_prompt_given = False
+        consecutive_identified = 0
+        while time.time() - start_time < recognize_train_timeout:
+            if time.time() - start_time > 10 and not center_prompt_given:
+                self.soundhandle.say('Make sure you are centered in the camera frame', voice, 1.0)
+                center_prompt_given = True
+
+            if self.latest_image.shape[0] > 1:
+                standing_person_face_encoding = self.detect_face(self.latest_image, gui=True)
+                if standing_person_face_encoding is None:
+                    consecutive_identified = 0
+                elif len(standing_person_face_encoding) > 1:
+                    consecutive_identified = 0
+                    if not only_person_prompt_given:
+                        self.soundhandle.say('Make sure you are the only person in the frame', voice, 1.0)
+                        only_person_prompt_given = True
+                else:
+                    consecutive_identified += 1
+                if consecutive_identified > 10:
+                    self.add_to_database(goal_handle.request.name, standing_person_face_encoding)
+                    cv2.waitKey(1)
+                    break
+
+            feedback_msg.running = True
+            goal_handle.publish_feedback(feedback_msg)
+            rclpy.spin_once(self)
+
+        self.training_action = False
+        cv2.destroyAllWindows()
+
+        result = RecognizeTrainRequest.Result()
+        if standing_person_face_encoding is not None:
+            goal_handle.succeed()
+            result.success = True
+        else:
+            goal_handle.abort()
+            result.success = False
+
+        return result
+
+    def detect_face(self, image, gui=False):
+        faces = self.face_cascade.detectMultiScale(self.latest_image_gray, 1.1, 4)
+        if gui:
+            image_mod = image.copy()
+            for (x, y, w, h) in faces:
+                cv2.rectangle(image_mod, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv2.imshow('img', image_mod)
+            cv2.waitKey(1)
+
+        standing_person_face_encoding = None
+        if len(faces) > 0:
+            if faces[0][2] > 100 and faces[0][3] > 100:
+                # tmp = face_recognition.face_encodings(image, known_face_locations=faces)
+                tmp = face_recognition.face_encodings(image)
+                if len(tmp) > 0:
+                    standing_person_face_encoding = tmp
+
+        return standing_person_face_encoding
+
+    def add_to_database(self, name, face_encoding):
+        path = os.path.join(save_path, name)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        path_images = os.path.join(path, 'images')
+        if not os.path.exists(path_images):
+            os.makedirs(path_images)
+        path_encodings = os.path.join(path, 'encodings')
+        if not os.path.exists(path_encodings):
+            os.makedirs(path_encodings)
+        num_files = len(os.listdir(path_encodings))
+        with open(os.path.join(path_encodings, 'encoding_' + str(num_files) + '.npy'), 'wb') as f:
+            np.save(f, face_encoding)
+
+        cv2.imwrite(os.path.join(path_images, 'image_' + str(num_files) + '.jpg'), self.latest_image)
+
+
+    def get_database(self):
+        person_list = os.listdir(save_path)
+        data_base = dict()
+        for person in person_list:
+            encoding_files = os.listdir(os.path.join(save_path, person))
+            for encoding_file in encoding_files:
+                with open(os.path.join(save_path, person, encoding_file), 'rb') as f:
+                    face_encoding = np.load(f)
+                    if person in data_base:
+                        data_base[person] = np.concatenate((data_base[person], face_encoding), axis=0)
+                    else:
+                        data_base[person] = face_encoding
+        return data_base
 
 
 def main(args=None):
@@ -62,7 +195,8 @@ def main(args=None):
 
     recognize_face_action_server = RecognizeFaceActionServer()
 
-    rclpy.spin(recognize_face_action_server)
+    while True:
+        rclpy.spin_once(recognize_face_action_server)
 
 
 if __name__ == '__main__':
