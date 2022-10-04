3b9f1df5d84446057b4bd111f3fed95e60380526
Paul Gesel <pac48@wildcats.unh.edu>
2022-10-03 23:38:09 -0400

    add read script and make call actions

--- shr_plan/launch/plansys2.launch.py
+++ shr_plan/launch/plansys2.launch.py
@@ -55,6 +55,18 @@ def generate_launch_description():
         name='recognize_face_action',
         output='screen')
 
+    read_script_node_cmd = Node(
+        package='pioneer_shr_py',
+        executable='read_script_action',
+        name='read_script_action',
+        output='screen')
+
+    make_call_node_cmd = Node(
+        package='pioneer_shr_py',
+        executable='make_call_action',
+        name='make_call_action',
+        output='screen')
+
     ld = LaunchDescription()
     ld.add_action(declare_namespace_cmd)
     ld.add_action(declare_use_sim_cmd)
@@ -64,6 +76,8 @@ def generate_launch_description():
     ld.add_action(unity_sim_cmd)
     ld.add_action(sound_node_cmd)
     ld.add_action(face_node_cmd)
+    ld.add_action(read_script_node_cmd)
+    ld.add_action(make_call_node_cmd)
 
     # actions
     move_cmd = Node(
