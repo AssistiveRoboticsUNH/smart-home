5b8799806f5a020a02c7fd539163311821cacc91
Paul Gesel <pac48@wildcats.unh.edu>
2022-10-06 18:26:01 -0400

    add actions for plansys

--- shr_plan/src/planning_controller_node.cpp
+++ shr_plan/src/planning_controller_node.cpp
@@ -27,112 +27,230 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 
-class PatrollingController : public rclcpp::Node
-{
-public:
-    PatrollingController()
-            : rclcpp::Node("plaining_controller"), state_(STARTING)
-    {
-    }
+#include <shr_plan_parameters.hpp>
+#include "std_msgs/msg/bool.hpp"
+#include "std_msgs/msg/string.hpp"
 
-    void init()
-    {
-        domain_expert_ = std::make_shared<plansys2::DomainExpertClient>();
-        planner_client_ = std::make_shared<plansys2::PlannerClient>();
-        problem_expert_ = std::make_shared<plansys2::ProblemExpertClient>();
-        executor_client_ = std::make_shared<plansys2::ExecutorClient>();
-        init_knowledge();
-    }
+namespace planning_controller {
 
-    void init_knowledge()
-    {
-        problem_expert_->addInstance(plansys2::Instance{"door", "landmark"});
-        problem_expert_->addInstance(plansys2::Instance{"home", "landmark"});
-        problem_expert_->addInstance(plansys2::Instance{"pioneer", "robot"});
-        problem_expert_->addInstance(plansys2::Instance{"midnight_warning", "message"});
-    }
+    struct World {
+        int pills_motion_sensor = -1;
+        int door_motion_sensor = -1;
+        int door_sensor = -1;
+    };
 
-    void init_predicates()
-    {
-//        for (const auto& pred : problem_expert_->getPredicates()){
-//            problem_expert_->removePredicate(pred);
-//        }
-        problem_expert_->addPredicate(plansys2::Predicate("(robot_at pioneer home)"));
-        problem_expert_->addPredicate(plansys2::Predicate("(message_at midnight_warning door)"));
+    class PlanningController : public rclcpp::Node {
+    public:
+        PlanningController()
+                : rclcpp::Node("planing_controller"), state_(IDLE) {
 
-    }
+            param_listener_ = std::make_shared<shr_plan_parameters::ParamListener>(get_node_parameters_interface());
+            params_ = param_listener_->get_params();
 
-    void step()
-    {
-        switch (state_) {
-            case STARTING:
-            {
-                init_predicates();
+            using std::placeholders::_1;
+            pill_motion_sub_ = this->create_subscription<std_msgs::msg::Bool>(
+                    params_.senor_pills_motion_topic, 10,
+                    std::bind(&PlanningController::pill_motion_callback, this, _1));
+            door_motion_sub_ = this->create_subscription<std_msgs::msg::Bool>(
+                    params_.sensors_door_motion_topic, 10,
+                    std::bind(&PlanningController::door_motion_callback, this, _1));
+            door_open_sub_ = this->create_subscription<std_msgs::msg::Bool>(
+                    params_.sensors_door_open_topic, 10, std::bind(&PlanningController::door_open_callback, this, _1));
 
-                // Set the goal for next state
-                problem_expert_->setGoal(plansys2::Goal("(and(robot_at pioneer door)(notified midnight_warning))"));
+            protocol_sub_ = this->create_subscription<std_msgs::msg::String>(
+                    params_.update_protocol_topic, 10,
+                    std::bind(&PlanningController::update_protocol_callback, this, _1));
 
-                // Compute the plan
-                auto domain = domain_expert_->getDomain();
-                auto problem = problem_expert_->getProblem();
-                auto plan = planner_client_->getPlan(domain, problem);
 
-                if (!plan.has_value()) {
-                    std::cout << "Could not find plan to reach goal " <<
-                              parser::pddl::toString(problem_expert_->getGoal()) << std::endl;
+        }
+
+        void init() {
+            domain_expert_ = std::make_shared<plansys2::DomainExpertClient>();
+            planner_client_ = std::make_shared<plansys2::PlannerClient>();
+            problem_expert_ = std::make_shared<plansys2::ProblemExpertClient>();
+            executor_client_ = std::make_shared<plansys2::ExecutorClient>();
+        }
+
+        void step() {
+            switch (state_) {
+                case IDLE: {
+                    if (!protocol_.empty()) {
+                        state_ = PLANNING;
+                    }
+                    break;
+                }
+                case PLANNING: {
+
+                    init_knowledge();
+                    init_predicates();
+                    set_goal();
+                    world_changed_ = false;
+
+                    // Compute the plan
+                    auto domain = domain_expert_->getDomain();
+                    auto problem = problem_expert_->getProblem();
+                    auto plan = planner_client_->getPlan(domain, problem);
+
+                    if (!plan.has_value()) {
+                        std::cout << "Could not find plan to reach goal " <<
+                                  parser::pddl::toString(problem_expert_->getGoal()) << std::endl;
+                        break;
+                    }
+
+                    // Execute the plan
+                    if (executor_client_->start_plan_execution(plan.value())) {
+                        state_ = EXECUTING;
+                    }
+
+                    break;
+                }
+                case EXECUTING: {
+                    if (world_changed_) {
+                        executor_client_->cancel_plan_execution();
+                    } else{
+                        auto feedback = executor_client_->getFeedBack();
+
+                        for (const auto &action_feedback: feedback.action_execution_status) {
+                            std::cout << "[" << action_feedback.action << " " <<
+                                      action_feedback.completion * 100.0 << "%]";
+                        }
+                        std::cout << std::endl;
+                    }
+
+                    if (!executor_client_->execute_and_check_plan() && executor_client_->getResult()) {
+                        if (executor_client_->getResult().value().success) {
+                            std::cout << "Successful finished " << std::endl;
+                            state_ = IDLE;
+                            protocol_ = "";
+                        } else {
+                            state_ = PLANNING;
+//                            for (const auto &action_feedback: feedback.action_execution_status) {
+//                                if (action_feedback.status == plansys2_msgs::msg::ActionExecutionInfo::FAILED) {
+//                                    std::cout << "[" << action_feedback.action << "] finished with error: " <<
+//                                              action_feedback.message_status << std::endl;
+//                                }
+//                                if (action_feedback.status == plansys2_msgs::msg::ActionExecutionInfo::CANCELLED) {
+//                                    std::cout << "[" << action_feedback.action << "] finished with cancel: " <<
+//                                              action_feedback.message_status << std::endl;
+//                                }
+//                            }
+                        }
+
+                    }
+
                     break;
                 }
 
-                // Execute the plan
-                if (executor_client_->start_plan_execution(plan.value())) {
-                    state_ = EXECUTING;
-                }
-
-                break;
-            }
-            case EXECUTING:
-            {
-                auto feedback = executor_client_->getFeedBack();
-
-                for (const auto & action_feedback : feedback.action_execution_status) {
-                    std::cout << "[" << action_feedback.action << " " <<
-                              action_feedback.completion * 100.0 << "%]";
-                }
-                std::cout << std::endl;
-
-                if (!executor_client_->execute_and_check_plan() && executor_client_->getResult()) {
-                    if (executor_client_->getResult().value().success) {
-                        std::cout << "Successful finished " << std::endl;
-                    } else {
-                        for (const auto & action_feedback : feedback.action_execution_status) {
-                            if (action_feedback.status == plansys2_msgs::msg::ActionExecutionInfo::FAILED) {
-                                std::cout << "[" << action_feedback.action << "] finished with error: " <<
-                                          action_feedback.message_status << std::endl;
-                            }
-                        }
-                    }
-                    state_ = STARTING;
-                }
-
-                break;
             }
         }
-    }
 
-private:
-    typedef enum {STARTING, EXECUTING} StateType;
-    StateType state_;
+    private:
+        void pill_motion_callback(const std_msgs::msg::Bool::SharedPtr msg) {
+            if (world_.pills_motion_sensor != msg->data) {
+                world_changed_ = true;
+            }
+            world_.pills_motion_sensor = msg->data;
+        }
 
-    std::shared_ptr<plansys2::DomainExpertClient> domain_expert_;
-    std::shared_ptr<plansys2::PlannerClient> planner_client_;
-    std::shared_ptr<plansys2::ProblemExpertClient> problem_expert_;
-    std::shared_ptr<plansys2::ExecutorClient> executor_client_;
-};
+        void door_motion_callback(const std_msgs::msg::Bool::SharedPtr msg) {
+            if (world_.door_motion_sensor != msg->data) {
+                world_changed_ = true;
+            }
+            world_.door_motion_sensor = msg->data;
+        }
 
-int main(int argc, char ** argv)
-{
+        void door_open_callback(const std_msgs::msg::Bool::SharedPtr msg) {
+            if (world_.door_sensor != msg->data) {
+                world_changed_ = true;
+            }
+            world_.door_sensor = msg->data;
+        }
+
+        void update_protocol_callback(const std_msgs::msg::String::SharedPtr msg) {
+            protocol_ = msg->data;
+        }
+
+        void init_knowledge() {
+            for (const auto &instance: problem_expert_->getInstances()) {
+                problem_expert_->removeInstance(instance);
+            }
+
+            if (protocol_ == "midnight_warning") {
+                problem_expert_->addInstance(plansys2::Instance{"door", "landmark"});
+                problem_expert_->addInstance(plansys2::Instance{"home", "landmark"});
+                problem_expert_->addInstance(plansys2::Instance{"pioneer", "robot"});
+                problem_expert_->addInstance(plansys2::Instance{"midnight_warning", "message"});
+            } else if (protocol_ == "take_pills") {
+                problem_expert_->addInstance(plansys2::Instance{"door", "landmark"});
+                problem_expert_->addInstance(plansys2::Instance{"home", "landmark"});
+                problem_expert_->addInstance(plansys2::Instance{"bedroom", "landmark"});
+                problem_expert_->addInstance(plansys2::Instance{"kitchen", "landmark"});
+
+                problem_expert_->addInstance(plansys2::Instance{"medicine_robot_msg", "message"});
+                problem_expert_->addInstance(plansys2::Instance{"medicine_phone_msg", "phonemessage"});
+
+                problem_expert_->addInstance(plansys2::Instance{"mediciness", "sensor"});
+
+//                door kitchen bedroom home - landmark
+//                medicine_robot_msg - message
+//                medicine_phone_msg - phonemessage
+//                mediciness - sensor
+            }
+
+        }
+
+        void init_predicates() {
+            for (const auto &pred: problem_expert_->getPredicates()) {
+                problem_expert_->removePredicate(pred);
+            }
+
+            if (protocol_ == "midnight_warning") {
+                problem_expert_->addPredicate(plansys2::Predicate("(robot_at pioneer home)"));
+                problem_expert_->addPredicate(plansys2::Predicate("(message_at midnight_warning door)"));
+            } else if (protocol_ == "take_pills") {
+                problem_expert_->addPredicate(plansys2::Predicate("(robot_at pioneer home)"));
+                problem_expert_->addPredicate(plansys2::Predicate("(message_at midnight_warning door)"));
+            }
+
+        }
+
+        void set_goal() {
+            if (protocol_ == "midnight_warning") {
+                problem_expert_->setGoal(plansys2::Goal("(and(robot_at pioneer door)(notified midnight_warning))"));
+            } else if (protocol_ == "take_pills") {
+                problem_expert_->setGoal(plansys2::Goal("(and(robot_at pioneer door)(notified midnight_warning))"));
+            }
+
+        }
+
+        typedef enum {
+            IDLE, PLANNING, EXECUTING
+        } StateType;
+        StateType state_;
+
+        std::shared_ptr<plansys2::DomainExpertClient> domain_expert_;
+        std::shared_ptr<plansys2::PlannerClient> planner_client_;
+        std::shared_ptr<plansys2::ProblemExpertClient> problem_expert_;
+        std::shared_ptr<plansys2::ExecutorClient> executor_client_;
+
+        std::shared_ptr<shr_plan_parameters::ParamListener> param_listener_;
+        shr_plan_parameters::Params params_;
+
+        rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr pill_motion_sub_;
+        rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr door_motion_sub_;
+        rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr door_open_sub_;
+        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr protocol_sub_;
+
+        World world_;
+        bool world_changed_ = false;
+        std::string protocol_ = "";
+    };
+
+}
+
+int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<PatrollingController>();
+    auto node = std::make_shared<planning_controller::PlanningController>();
 
     node->init();
 
